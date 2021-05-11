0c00a70a190b1fdee7f322e045ee690d1eada476
Xsatu <cassanovarevayanto@gmail.com>
2021-05-12 05:47:48 +0700

    menambah javascript

---
+++ javaScript.js
@@ -0,0 +1,7 @@
+var confirm = confirm('Klik OK untuk melanjutkan');
+
+if(confirm === true) {
+    window.location = "https://xsatu.github.io/Program-Python-sederhana2.github.io/";
+} else {
+    window.location = "https://www.google.com/";
+}
