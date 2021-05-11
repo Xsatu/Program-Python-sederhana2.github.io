eec8323bf0c37a86b1cfa14c634f011f2f61de3e
Xsatu <cassanovarevayanto@gmail.com>
2021-05-12 05:56:24 +0700

    Remove javascript

--- javaScript.js
+++
@@ -1,7 +0,0 @@
-var confirm = confirm('Klik OK untuk melanjutkan');
-
-if(confirm === true) {
-    window.location = "https://xsatu.github.io/Program-Python-sederhana2.github.io/";
-} else {
-    window.location = "https://www.google.com/";
-}
